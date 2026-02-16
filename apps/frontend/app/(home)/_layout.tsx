23ea426cebc63130d7f4f5d7e374a59048e20290
AlessandroB1298 <aidanbongiorno09@gmail.com>
2026-02-15 23:35:59 -0500

    Database connection & restructure
    * Created a db connection using convex db
    * Removed backend folder to adhere to a more modular code base
    * added a test function in home.tsx to test the convex db tasks function

--- apps/frontend/app/(home)/_layout.tsx
+++ apps/frontend/app/(home)/_layout.tsx
@@ -1,16 +1,23 @@
 import { NativeTabs, Icon, Label } from "expo-router/unstable-native-tabs";
+import { ConvexProvider, ConvexReactClient } from "convex/react";
+
+const convex = new ConvexReactClient(process.env.EXPO_PUBLIC_CONVEX_URL!, {
+  unsavedChangesWarning: false,
+});
 
 export default function TabLayout() {
   return (
-    <NativeTabs>
-      <NativeTabs.Trigger name="index">
-        <Label>Home</Label>
-        <Icon sf="house.fill" drawable="custom_android_drawable" />
-      </NativeTabs.Trigger>
-      <NativeTabs.Trigger name="settings">
-        <Icon sf="gear" drawable="custom_settings_drawable" />
-        <Label>Settings</Label>
-      </NativeTabs.Trigger>
-    </NativeTabs>
+    <ConvexProvider client={convex}>
+      <NativeTabs>
+        <NativeTabs.Trigger name="index">
+          <Label>Home</Label>
+          <Icon sf="house.fill" drawable="custom_android_drawable" />
+        </NativeTabs.Trigger>
+        <NativeTabs.Trigger name="settings">
+          <Icon sf="gear" drawable="custom_settings_drawable" />
+          <Label>Settings</Label>
+        </NativeTabs.Trigger>
+      </NativeTabs>
+    </ConvexProvider>
   );
 }
--- apps/frontend/app/(home)/index.tsx
+++ apps/frontend/app/(home)/index.tsx
@@ -1,32 +1,20 @@
-import { testClerkApi } from "@/api/helperFuncs";
-import { TButton } from "@/components/themedComponents/themed-button";
 import { TText } from "@/components/themedComponents/themed-text";
 import { TView } from "@/components/themedComponents/themed-view";
-import { useAuth } from "@clerk/clerk-expo";
-import { useState } from "react";
+import { api } from "@/convex/_generated/api";
+import { useQuery } from "convex/react";
+import { Text } from "react-native";
 
 export default function HomeScreen() {
-  const { getToken } = useAuth();
-  const [data, setData] = useState<string>("");
+  const tasks = useQuery(api.tasks.get);
 
   return (
     <TView className="flex-1 items-center justify-center">
       <TText type="default">This is the home screen</TText>
-      <TButton
-        type="secondary"
-        onPress={() => {
-          getToken().then((tok)=>{
-            if(tok){
-              testClerkApi(tok).then((res)=>{
-                setData(res);
-              })
-            }
-          })
-        }}
-      >
-        <TText type="secondary">Test Clerk api</TText>
-      </TButton>
-      <TView>{data && <TText>{data}</TText>}</TView>
+      {tasks?.map(({ _id, text }) => (
+        <Text className="text-white text-md" key={_id}>
+          {text}
+        </Text>
+      ))}
     </TView>
   );
 }
