197161ccc2ef9b8b6025ee1d656899c537891bcd
Alessandro b <98426727+AlessandroB1298@users.noreply.github.com>
2026-04-06 12:11:49 -0400

    Update businessFollowers in test cases to include 'user1'

--- __tests__/convex/businesses.test.tsx
+++ __tests__/convex/businesses.test.tsx
@@ -52,7 +52,7 @@ describe("Business mutations and queries", () => {
         businessLocation: "Detroit, MI",
         businessId: "business-001",
         created_by: "user-001",
-		businessFollowers : [],
+		businessFollowers : ["user1"],
       };
 
       const result = await createBusinessHandler(ctx, args);
@@ -62,7 +62,7 @@ describe("Business mutations and queries", () => {
         businessId: "business-001",
         businessLocation: "Detroit, MI",
         created_by: "user-001",
-		businessFollowers : [],
+		businessFollowers : ["user1"],
       });
       expect(result).toBe(fakeId);
     });
@@ -77,7 +77,7 @@ describe("Business mutations and queries", () => {
           businessLocation: "Detroit, MI",
           businessId: "business-001",
           created_by: "user-001",
-		  businessFollowers : [],
+		  businessFollowers : ["user1"],
 
         })
         ).rejects.toThrow("DB insert failed");
@@ -91,7 +91,7 @@ describe("Business mutations and queries", () => {
 			businessId: "business-001",
 			created_by: "user-001",
 			businessLogo: "https://example.com/logo.png",
-			businessFollowers : [],
+			businessFollowers : ["user1"],
 
 		};
 
@@ -106,7 +106,7 @@ describe("Business mutations and queries", () => {
 				businessName: "Test Name",
 				businessLocation: "Detroit, MI",
 				businessLogo: "https://example.com/logo.png",
-				businessFollowers : [],
+				businessFollowers : ["user1"],
 			});
 		});
 
@@ -127,7 +127,7 @@ describe("Business mutations and queries", () => {
 				_id: "id-1",
 				created_by: "user-001",
 				businessName: "Test Name",
-				businessFollowers : [],
+				businessFollowers : ["user1"],
 
 			};
 			mocks.mockUnique.mockResolvedValue(fakeBusiness);
